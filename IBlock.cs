2b2e851fe1c81586c3878f330de7aa03af19a934
JokarTheFirst <142092410+JokarTheFirst@users.noreply.github.com>
2024-06-07 16:53:54 +0300

    Created IBlock class

---
+++ IBlock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisGame
+{
+    public class IBlock : Block
+    {
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] { new(1,0), new(1,1), new(1,2), new(1,3) },
+            new Position[] { new(1,0), new(1,1), new(1,2), new(1,3) },
+            new Position[] { new(1,0), new(1,1), new(1,2), new(1,3) },
+            new Position[] { new(1,0), new(1,1), new(1,2), new(1,3) }
+        };
+    }
+}
