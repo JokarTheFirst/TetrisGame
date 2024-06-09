1df27852237d81c290d245d618790001f374abe8
JokarTheFirst <142092410+JokarTheFirst@users.noreply.github.com>
2024-06-09 18:41:26 +0300

    Shifted Screen upward

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -70,7 +70,7 @@ namespace TetrisGame
                         Height = cellSize,
                     };
 
-                    Canvas.SetTop(imageControl, (r - 2) * cellSize);
+                    Canvas.SetTop(imageControl, (r - 2) * cellSize + 10);
                     Canvas.SetLeft(imageControl, c*cellSize);
                     GameCanvas.Children.Add(imageControl);
                     imageControls[r,c] = imageControl;
