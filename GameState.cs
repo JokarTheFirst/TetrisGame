f859b44b468c8179904598c7b35d53b7f2397244
JokarTheFirst <142092410+JokarTheFirst@users.noreply.github.com>
2024-06-09 18:39:45 +0300

    Shifted Down Blokc Spawn Positions

--- GameState.cs
+++ GameState.cs
@@ -11,7 +11,19 @@
             {
                 currentBlock = value;
                 currentBlock.Reset();
+
+                for (int i = 0; i < 2; i++)
+                {
+                    currentBlock.Move(1, 0);
+
+                    if (!BlockFits())
+                    {
+                        currentBlock.Move(-1,0);
+                    }
+                }
             }
+
+
         }
 
         public GameGrid GameGrid { get; }
